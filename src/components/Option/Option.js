2ad8fee7985b2715a96a00f22ce4da54a419d56d
FerdousFardin <fardinh39u@gmaile.com>
2022-10-12 19:06:53 +0600

    changes reverted

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -3,13 +3,16 @@ import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
-export const Option = ({ option, correctAnswer, index }) => {
-  const [optionStyle, setOptionStyle] = useState("neutral");
+export const Option = ({ option, correctAnswer }) => {
+  const [optionStyle, setOptionStyle] = useState("border-neutral");
   const validateAnswer = (answer) => {
+    setOptionStyle("border-neutral");
     const validation = answer === correctAnswer;
     const toastType = validation ? toast.success : toast.error;
     const text = validation ? "You are correct!" : "You are wrong!";
-    setOptionStyle(validation ? "success" : "error");
+    setOptionStyle(
+      validation ? "border-success text-success" : "border-error text-error"
+    );
     toastType(text, {
       position: "top-right",
       autoClose: 5000,
@@ -21,28 +24,15 @@ export const Option = ({ option, correctAnswer, index }) => {
       theme: "colored",
     });
   };
-  // console.log(`option-${index}`, option);
   return (
-    <div
-      onClick={() => validateAnswer(option)}
-      // onBlur={() => setOptionStyle("neutral")}
-      className={`flex items-center gap-2 border-2 border-${optionStyle} text-${optionStyle} duration-300 ease-linear p-4 rounded text-lg lg:text-xl`}
-    >
-      <input
-        type="radio"
-        className={`radio radio-sm checked:bg-${
-          optionStyle === "success"
-            ? optionStyle
-            : optionStyle === "neutral"
-            ? "primary"
-            : "[#e74c3c]"
-        } duration-300 ease-linear`}
-        name={`option-${index}`}
-        id={option}
-      />
-      <label className="" htmlFor={option}>
+    <>
+      <button
+        onClick={() => validateAnswer(option)}
+        className={`border-2 ${optionStyle} p-4 rounded text-lg lg:text-xl flex items-center gap-2`}
+      >
+        <CheckCircleIcon className="w-6 h-6" />
         {option}
-      </label>
-    </div>
+      </button>
+    </>
   );
 };
